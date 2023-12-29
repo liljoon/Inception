6e181e41750d2601dc66747c8b887d82a46c997d
sunwooiljoon <wnsdl1659@naver.com>
2023-12-29 21:15:55 +0900

    feat: tls + https 설정완료

--- src/requirements/nginx/tools/script.sh
+++ src/requirements/nginx/tools/script.sh
@@ -1,1 +1,6 @@
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-key.pem -out /etc/ssl/certs/nginx-certificate.pem \
+--subj "/C=KR/ST=Seoul/L=Gangnam/O=42Seoul/OU=cadet/CN=${DOMAIN_NAME}"
+
+envsubst '$DOMAIN_NAME' < /etc/nginx/http.d/default.template > /etc/nginx/http.d/default.conf
+
 usr/sbin/nginx -g "daemon off;"
