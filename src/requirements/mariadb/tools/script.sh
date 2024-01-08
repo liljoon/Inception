63c38d7d31bcc340ee49d87ab960e92f5a9a03d8
isunwoo <isunwoo@student.42seoul.kr>
2024-01-08 19:34:43 +0900

    feat : sleep 빼고 실행

--- src/requirements/mariadb/tools/script.sh
+++ src/requirements/mariadb/tools/script.sh
@@ -1,14 +1,9 @@
-service mariadb start
-
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" > db.sql
+echo "FLUSH PRIVILEGES;" >> db.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> db.sql
 echo "GRANT ALL PRIVILEGES ON $DB_USER.* TO '$DB_USER'@'%';" >> db.sql
 echo "FLUSH PRIVILEGES;" >> db.sql
 
-mariadb < /db.sql
-
-sleep 1
-
-service mariadb stop
+mariadbd --bootstrap < /db.sql
 
 mariadbd-safe
